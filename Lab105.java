3a0c91a812d4c8af3b378e97e5b069c9973373b5
for7lwv <forgouwaijo@gmail.com>
2025-11-17 17:40:38 +0700

    Add files via upload

---
+++ Lab105.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+
+public class Lab105 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+         "Summation number = " + (2+5) +
+                 "\nSubtraction number = " + (2-5) +
+                 "\nmultiplication number = " + (2*5)+
+                 "\nDivison number = " + (2/5) +
+                 "\nModulus number = " + (2%5),
+                 "Arithmetic Operators",
+                 JOptionPane.WARNING_MESSAGE);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                
+        
+    }
+}
